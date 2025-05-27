b93abb90a925604e6ece06ef3f83f3675ea2d11c
UmarAyaz23 <umarayaz3321@gmail.com>
2025-05-27 19:50:15 +0500

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const Start());
 }
 
+List<String> cartItems = [];
+
 class Start extends StatelessWidget {
   const Start({super.key});
 
@@ -85,7 +87,7 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
 
                   if (correct) {
                     Navigator.pushReplacement(
-                        context, MaterialPageRoute(builder: (context) => HomePage()));
+                        context, MaterialPageRoute(builder: (context) => const HomePage()));
                   }
                 },
                 style: ElevatedButton.styleFrom(
@@ -103,9 +105,14 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
   }
 }
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<String> drawerOptions = ["Find a Book", "Issue a Book", "Publish a Book"];
 
   @override
@@ -115,10 +122,19 @@ class HomePage extends StatelessWidget {
         title: const Text("MAD BookList Quiz", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
         backgroundColor: Colors.blueGrey,
         centerTitle: true,
+        actions: [
+          Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () => Scaffold.of(context).openEndDrawer(),
+            ),
+          )
+        ],
       ),
+
       drawer: Drawer(
         child: Container(
-          color: Colors.blueGrey,
+          color: Colors.white,
           child: Column(
             children: [
               const SizedBox(height: 30),
@@ -132,32 +148,104 @@ class HomePage extends StatelessWidget {
                   shrinkWrap: true,
                   itemCount: drawerOptions.length,
                   itemBuilder: (context, int index) {
-                    return ListTile(
-                      title: Text(drawerOptions[index], style: const TextStyle(color: Colors.white)),
-                      onTap: () {
-                        Navigator.pop(context); 
-                        if (drawerOptions[index] == "Issue a Book") {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => IssueBookPage()));
-                        } else {
-                          ScaffoldMessenger.of(context)
-                              .showSnackBar(const SnackBar(content: Text("Feature Unimplemented")));
-                        }
-                      },
+                    return Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.blueGrey, width: 1),
+                        ),
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(5),
+                        title: Text(
+                          drawerOptions[index],
+                          style: const TextStyle(color: Colors.blueGrey),
+                        ),
+                        onTap: () {
+                          Navigator.pop(context);
+                          if (drawerOptions[index] == "Issue a Book") {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => IssueBookPage(onItemAdded: () {
+                                  setState(() {});
+                                }),
+                              ),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text("Feature Unimplemented")),
+                            );
+                          }
+                        },
+                      ),
                     );
                   },
                 ),
-              ),
+              )
             ],
           ),
         ),
       ),
+
+      endDrawer: Drawer(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text("Cart",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blueGrey,),),
+              ),
+              SizedBox(height: 20),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: cartItems.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.blueGrey, width: 1),
+                        ),
+                      ),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(5),
+                        title: Text(cartItems[index], style: TextStyle( fontSize: 16, fontWeight: FontWeight.w300, color: Colors.black,),
+                        ),
+                      )
+                    );
+                  },
+                ),
+              ),
+
+              SizedBox(height: 20,),
+              TextButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text("Book Options"),
+                        content: Text("Thank you for your purchase"),
+                      );
+                    },
+                  );
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                child: const Text("Checkout"),
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
 
 class IssueBookPage extends StatelessWidget {
-  IssueBookPage({super.key});
+  final VoidCallback onItemAdded;
+  IssueBookPage({super.key, required this.onItemAdded});
 
   final List<String> bookList = ["Book 01", "Book 02", "Book 03", "Book 04", "Book 05", "Book 06", "Book 07", "Book 08"];
 
@@ -180,10 +268,14 @@ class IssueBookPage extends StatelessWidget {
                 builder: (BuildContext context) {
                   return AlertDialog(
                     title: const Text("Book Options"),
-                    content: const Text("Would you like to add to cart?"),
+                    content: Text("Would you like to ${bookList[index]} add to cart?"),
                     actions: [
                       TextButton(
-                        onPressed: () => Navigator.pop(context),
+                        onPressed: () {
+                          cartItems.add(bookList[index]);
+                          onItemAdded();
+                          Navigator.pop(context);
+                        },
                         child: const Text("Add to Cart"),
                       ),
                       TextButton(
